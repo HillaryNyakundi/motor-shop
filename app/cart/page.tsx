50c87a3b8ba8eb23283137a1760fcdf682b96a7d
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-06-20 18:41:33 +0300

    I created the cart UI

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,14 @@
+import Container from "../Container";
+import CartClient from "./CartClient";
+
 const Cart = () => {
-  return <div>Cart Page</div>;
+  return (
+    <div className="pt-8">
+      <Container>
+        <CartClient />
+      </Container>
+    </div>
+  );
 };
 
 export default Cart;
