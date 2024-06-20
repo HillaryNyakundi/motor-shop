ac1c957c122d6ee342a510a6ec9a2a6fe952f67e
HillaryNyakundi <hillarynyakundi66@gmail.com>
2024-06-20 14:41:21 +0300

    Enabled persistence of the items that are already added to cart

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,5 +1,11 @@
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
-import { createContext, useCallback, useContext, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 type CartContextType = {
   cartTotalQty: number;
@@ -19,6 +25,13 @@ export const CartContextProvider = (props: Props) => {
     null
   );
 
+  useEffect(() => {
+    const cartItems: any = localStorage.getItem("motorShopItems");
+    const cProducts: CartProductType[] | null = JSON.parse(cartItems);
+
+    setCartProducts(cProducts);
+  }, []);
+
   const handleAddProductToCart = useCallback((product: CartProductType) => {
     setCartProducts((prev) => {
       let updatedCart;
@@ -29,6 +42,7 @@ export const CartContextProvider = (props: Props) => {
         updatedCart = [product];
       }
 
+      localStorage.setItem("motorShopItems", JSON.stringify(updatedCart));
       return updatedCart;
     });
   }, []);
